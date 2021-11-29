6ac86ae5abaf534bfb524ecf50b4085682289bbd
Jacob Ramsey <jacob@techmonkey.pro>
2021-11-29 12:47:36 -0600

    comment out git configs

--- 1_basics.sh
+++ 1_basics.sh
@@ -2,5 +2,5 @@ yum install -y git openssh wget sudo bash-completion vim unzip
 
 ssh-keygen #and copy key to github
 
-git config --global user.email "jacob@techmonkey.pro"
-git config --global user.name "Jacob Ramsey"
+#git config --global user.email "jacob@techmonkey.pro"
+#git config --global user.name "Jacob Ramsey"
