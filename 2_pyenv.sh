a15db19799611c64ba09f4786df327c0a1f08fe1
Jacob Ramsey <jacob@techmonkey.pro>
2021-11-29 12:44:12 -0600

    Remove un-needed

--- 2_pyenv.sh
+++ 2_pyenv.sh
@@ -18,3 +18,5 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+exec $SHELL
--- 2b_pyenv.sh
+++
@@ -1,9 +0,0 @@
-#Reload shell
-exec $SHELL
-
-pyenv --version
-
-pyenv install 3.9.9
-pyenv local 3.9.9
-
-python --version
